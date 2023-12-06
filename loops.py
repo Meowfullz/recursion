52068001cc63bf3df060263a18e162d3650188b5
Gooby <129816575+Meowfullz@users.noreply.github.com>
2023-12-06 10:35:08 -0500

    12/6

--- loops.py
+++ loops.py
@@ -56,3 +56,60 @@ class loops:
 			i = i - 1
 
 		return result
+	
+	@staticmethod
+	def computeMin(nums):
+		"""Finds the minimum number in a specified list of numbers
+		
+
+		Args:
+			nums: specified list
+
+		Returns:
+			int: minimum number
+		"""
+
+		# the counter variable of the loop is the 
+		# index used to iterate through the elements in the list 
+
+		# the loop will repeat as long as i < len(nums
+		# the condition that will cause the loop to stop
+		# is i == len(nums) -> stopping case for the loop
+
+		# with each iteration of the loop we're incrementing
+		# the counter variable by 1
+
+		# when the loop stops, it returns min
+
+		i = 0
+		min = nums[i]
+
+		while (i < len(nums)):
+			if (nums[i] <= min):
+				min = nums[i]
+			i += 1
+		
+		return min
+	
+	@staticmethod
+	def reverse(s:str):
+		"""displays a specified string in reverse
+
+		Args:
+			s (str): specified str
+		"""
+
+		# the counter variable in the loop is the index
+		# used to iterate through the characters in the string
+
+		# the stopping case for the loop is i == 0
+
+		# with each iteration of the loop, we're 
+		# decrementing the counter variable
+
+		 # when the loop stops, we're printing a message
+		i = len(s)
+		while (i > 0):
+			print(s[i-1], end='')
+			i -= 1
+		print(' is the reverse of %s using a loop' % (s))
